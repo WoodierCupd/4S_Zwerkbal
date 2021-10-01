738818000cac13b868834e1bee8a930834f55307
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2021-10-01 11:11:27 +0200

    opdracht 1

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -1,2 +1,17 @@
-<h1>test</h1>
+@extends('layout')
+
+@section('nav')
+    <nav>
+        <a href="/" class="active">Home</a>
+        <a href="/tournaments">Toernooien</a>
+        <a href="/teams">Teams</a>
+        <a href="/players">Spelers</a>
+    </nav>
+@endsection
+@section('content')
+
+    <h1>teams</h1>
+
+@endsection
+
 <!-- hier komt een lijst van teams -->
